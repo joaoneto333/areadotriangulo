bfe9a26994b10050603b4eb31c4f8c0fc13e9485
joaoneto333 <56695005+joaoneto333@users.noreply.github.com>
2019-10-17 14:45:30 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	int b,h;
+	printf("Digite a base do Triangulo:\n");
+	scanf("%d",&b);
+	printf("Digite a altura do Triangulo:\n");
+	scanf("%d",&h);
+	
+	printf("A area do triangulo equivale: %d",(b*h)/2);
+}
